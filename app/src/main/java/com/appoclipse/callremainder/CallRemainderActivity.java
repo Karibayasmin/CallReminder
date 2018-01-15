4f8b57f1d9c434c0ee73c8b4edc44d82dfb7ea33
mrshuvo <mr.shahriyarshuvo@GMAIL.COM>
2018-01-15 22:14:24 +0600

    Initial commit

---
+++ app/src/main/java/com/appoclipse/callremainder/CallRemainderActivity.java
@@ -0,0 +1,13 @@
+package com.appoclipse.callremainder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CallRemainderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call_remainder);
+    }
+}
---
+++ app/src/main/java/com/appoclipse/callremainder/splashScreen.java
@@ -0,0 +1,30 @@
+package com.appoclipse.callremainder;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+public class splashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        final ProgressBar bar = (ProgressBar) findViewById(R.id.loading);
+
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent i = new Intent(getApplicationContext(), CallRemainderActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },2500);
+    }
+}
