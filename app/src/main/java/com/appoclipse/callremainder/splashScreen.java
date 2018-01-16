a46eb5e48671684b912efef459664b85d1e519c0
mrshuvo <mr.shahriyarshuvo@GMAIL.COM>
2018-01-17 00:44:58 +0600

    Splash Screen Updated!

--- app/src/main/java/com/appoclipse/callremainder/splashScreen.java
+++ app/src/main/java/com/appoclipse/callremainder/splashScreen.java
@@ -4,18 +4,45 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.TransitionManager;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class splashScreen extends AppCompatActivity {
+
+    TextView appname, tag;
+    ImageView applogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Switching to full screen mode
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
 
         final ProgressBar bar = (ProgressBar) findViewById(R.id.loading);
 
+        //Animation
+        appname = findViewById(R.id.appname);
+        applogo = findViewById(R.id.logo);
+        tag = findViewById(R.id.tag);
 
+        appname.animate().alpha(0f).setDuration(500);
+        appname.animate().translationXBy(-1000f).setDuration(3000);
+        applogo.animate().alpha(0f).setDuration(500);
+        applogo.animate().translationXBy(1000f).setDuration(3000);
+        tag.animate().alpha(1f).setDuration(3000);
+
+        //Switching to Main Activity
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
